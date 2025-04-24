0ff1209f8af007799b8765f9011c2e42a008a265
Gleyberson <g.ruiz_19@hotmail.com>
2025-04-23 21:16:37 -0400

    change index

---
+++ main.js
@@ -0,0 +1,25 @@
+// tipos de variables
+
+// string
+let nombre = "Juan"
+
+// number
+let edad = 33
+
+// boolean
+let isLearning = true
+
+// constantes
+
+const IVA = 0.21; // 21% de IVA
+const nacimiento = 1990; // año de nacimiento
+
+console.log("IVA: "+IVA)
+console.log("nacimiento: "+nacimiento)
+console.log("nombre: "+nombre)
+console.log("edad: "+edad)
+console.log("isLearning: "+isLearning)
+
+let title = document.getElementById("title").innerHTML = "Hola Mundo desde JS"
+console.log(title)
+
