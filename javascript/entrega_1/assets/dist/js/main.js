7cb52c5a58f2289094d7d32e4c6019946de8d1d8
Gleyberson <g.ruiz_19@hotmail.com>
2025-05-29 00:10:18 -0400

    entrega v1

---
+++ javascript/entrega_1/assets/dist/js/main.js
@@ -0,0 +1,108 @@
+let categories = [
+    { tipo: 'ingreso', nombre: 'Sueldo', monto: 0 },
+    { tipo: 'ingreso', nombre: 'Trabajo extra', monto: 0 },
+    { tipo: 'gasto', nombre: 'Alquiler', monto: 0 },
+    { tipo: 'gasto', nombre: 'Comida', monto: 0 }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(categories.length);
+    let tiposUnicos = [];
+    for (let i = 0; i < categories.length; i++) {
+        if (!tiposUnicos.includes(categories[i].tipo)) {
+            console.log(categories[i].tipo);
+            tiposUnicos.push(categories[i].tipo);
+            renderTable(categories[i].tipo);
+        }
+    }   
+    document.querySelectorAll('.monto-input').forEach(input => {
+        input.addEventListener('keyup', calcularTotales);
+    });
+});
+function renderTable(tipo) {
+    console.log('Renderizando tabla para tipo:', tipo);
+    const tbody = document.querySelector('#tablecat'+tipo+' tbody');
+
+    tbody.innerHTML = ''; 
+
+    categories
+        .filter(cat => cat.tipo === tipo)
+        .forEach((cat, index) => {
+            const tr = document.createElement('tr');
+
+            tr.innerHTML = `
+                <th>${cat.nombre}</th>
+                <td>
+                    <div class="input-group">
+                        <span class="input-group-text">
+                            <i class="bi bi-currency-dollar"></i>
+                        </span>
+                        <input type="number" class="form-control monto-input" data-tipo="${cat.tipo}" value="${cat.monto}" onchange="updateMonto(${index}, this.value)">
+                    </div>
+                </td>
+                <td><button class="btn btn-danger" onclick="eliminarItem(${index})">Eliminar</button></td>
+            `;
+
+            tbody.appendChild(tr);
+        });
+}
+function updateMonto(index, value) {
+    categories[index].monto = parseFloat(value) || 0;
+}
+
+function eliminarItem(index) {
+    categories.splice(index, 1);
+    renderTable('ingreso'); // o 'gasto', según lo que estés viendo
+}
+
+function agregarItem(tipo, nombre) {
+    console.log('Agregando item tipo:', tipo);
+    console.log('Agregando item nombre:', nombre);
+    let cat = categories.find(cat => cat.tipo === tipo && cat.nombre === nombre)
+    console.log(cat);
+    if (!nombre || nombre.trim() === '') {
+        alert('El nombre no puede estar vacío.');
+        return;
+    }
+    if (cat) {
+        alert('Ya existe un elemento con este nombre en esta categoría.');
+        return;
+    } else {
+        categories.push({ tipo, nombre, monto: 0 });
+    }
+    renderTable(tipo);
+}
+function calcularTotales (){
+    let total = {
+        ingreso: 0,
+        gasto: 0
+    };
+    let inputs = document.querySelectorAll('.monto-input');
+
+    inputs.forEach(input => {
+        let tipo = input.dataset.tipo;
+        let valor = parseFloat(input.value) || 0;
+
+        console.log('Tipo:', tipo, 'Valor:', valor);
+
+        if (total.hasOwnProperty(tipo)) {
+            total[tipo] += valor;
+        } else {
+            total[tipo] = valor;
+        }
+    });
+    let ahorro = total.ingreso - total.gasto;
+    // Mostrar resultados
+    document.getElementById('total-ingreso').textContent = total.ingreso.toLocaleString('es-AR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+    document.getElementById('total-gasto').textContent = total.gasto.toLocaleString('es-AR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+    document.getElementById('total-ahorro').textContent = ahorro.toLocaleString('es-AR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+}
